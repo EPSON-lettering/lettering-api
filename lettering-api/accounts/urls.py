057093d410541208a7edff0549d51b05f1fc27ea
minesp3164 <lhd000721@gmail.com>
2024-06-12 04:14:38 +0900

    chore: models.py 코드 수정

--- lettering-api/accounts/urls.py
+++ lettering-api/accounts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
@@ -16,7 +16,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('nickname/check/', views.NicknameCheckView.as_view(), name='nickname_check'),
-    path('swagger(?P<format>\.json|\.yaml)$',schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path('swagger(?P<format>\.json|\.yaml)$',schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
--- lettering-api/matching/models.py
+++ lettering-api/matching/models.py
@@ -1,12 +1,10 @@
 from django.db import models
-from accounts.models import User, Language
+from accounts.models import User
 
 
 class Match(models.Model):
     requester = models.ForeignKey(User, related_name='requested_matches', on_delete=models.CASCADE)
     acceptor = models.ForeignKey(User, related_name='accepted_matches', on_delete=models.CASCADE)
-    native_lang = models.ForeignKey(Language, related_name='native_matches', on_delete=models.CASCADE)
-    learning_lang = models.ForeignKey(Language, related_name='learning_matches', on_delete=models.CASCADE)
     state = models.BooleanField()
     created_at = models.DateTimeField(auto_now_add=True)
     withdraw_reason = models.TextField(null=True)
