4de15dae55e20871ffd6c73982020da474d3491f
west_east <west.east1832@gmail.com>
2024-06-15 04:21:13 +0900

    fix: CORS Error

--- lettering-api/app/settings.py
+++ lettering-api/app/settings.py
@@ -135,6 +135,6 @@ AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
 
 CORS_ALLOWED_ORIGINS = [
     "http://galaxy4276.asuscomm.com:3000"
+    "http://localhost:3000",
+    "http://localhost:8000",
 ]
-
-CORS_ALLOW_CREDENTIALS = True
