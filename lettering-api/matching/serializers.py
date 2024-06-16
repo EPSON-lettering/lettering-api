1ee5e54a5ce16260bceb97ce7b05928cc0b46150
west_east <west.east1832@gmail.com>
2024-06-17 01:08:51 +0900

    fix: 매칭 시스템 관련 응답 카멜케이스 적용

--- lettering-api/matching/serializers.py
+++ lettering-api/matching/serializers.py
@@ -3,22 +3,29 @@ from .models import MatchRequest, Match
 from accounts.models import User
 
 class MatchUserSerializer(serializers.ModelSerializer):
+    profileImageUrl = serializers.CharField(source='profile_image_url')
+
     class Meta:
         model = User
-        fields = ['id', 'nickname', 'profile_image_url', 'language']
+        fields = ['id', 'nickname', 'profileImageUrl', 'language']
 
 class MatchRequestSerializer(serializers.ModelSerializer):
     requester = MatchUserSerializer()
     receiver = MatchUserSerializer()
+    createdAt = serializers.DateTimeField(source='created_at')
 
     class Meta:
         model = MatchRequest
-        fields = ['id', 'requester', 'receiver', 'state', 'created_at']
+        fields = ['id', 'requester', 'receiver', 'state', 'createdAt']
 
 class MatchSerializer(serializers.ModelSerializer):
     requester = MatchUserSerializer()
     acceptor = MatchUserSerializer()
+    nativeLang = serializers.CharField(source='native_lang')
+    learningLang = serializers.CharField(source='learning_lang')
+    createdAt = serializers.DateTimeField(source='created_at')
+    withdrawReason = serializers.CharField(source='withdraw_reason', allow_null=True, allow_blank=True)
 
     class Meta:
         model = Match
-        fields = ['requester', 'acceptor', 'state', 'native_lang', 'learning_lang', 'created_at', 'withdraw_reason']
+        fields = ['requester', 'acceptor', 'state', 'nativeLang', 'learningLang', 'createdAt', 'withdrawReason']
