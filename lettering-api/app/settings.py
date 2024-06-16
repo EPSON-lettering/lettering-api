d14323de4d42f8e27cc0324178eb736a99312769
west_east <west.east1832@gmail.com>
2024-06-16 15:59:15 +0900

    fix: 토큰 기간 연장 및 토큰 재발급 url추가

--- lettering-api/app/settings.py
+++ lettering-api/app/settings.py
@@ -90,7 +90,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=120),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
