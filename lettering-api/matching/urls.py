9f1f4585578a1f28ce801a6e432aa271109ae24b
galaxy4276 <deveungi@gmail.com>
2024-06-17 20:34:18 +0900

    fix: 마이그레이션 정보 누락

--- lettering-api/matching/urls.py
+++ lettering-api/matching/urls.py
@@ -9,7 +9,6 @@ from .views import (
 from django.urls import path, re_path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
-from django.urls import path
 from rest_framework.permissions import AllowAny
 
 schema_view = get_schema_view(
