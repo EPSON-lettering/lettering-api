0d29d1d9696d05224371b1f29cdaeef4ae37ec90
minesp3164 <lhd000721@gmail.com>
2024-06-21 00:05:16 +0900

    chore: 스캔한 이미지 혹은 올린 이미지 전송하기 API

--- lettering-api/printers/serializers.py
+++ lettering-api/printers/serializers.py
@@ -1,20 +1,58 @@
-from django.core.files import File
+import boto3
+import os
+from matching.models import Match
+from botocore.exceptions import ClientError
 from rest_framework import serializers
-from rest_framework.fields import empty
-
-from .models import EpsonConnectScanData
-from accounts.models import User
-import base64
+from letters.models import Letter
 
 
 class EpsonConnectPrintSerializer(serializers.Serializer):
     imageFile = serializers.ImageField(required=True)
 
 
-class EpsonScanSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = EpsonConnectScanData
-        fields = '__all__'
+class EpsonScanSerializer(serializers.Serializer):
+    imagefile = serializers.FileField()
+
+    def create(self, validated_data):
+        imagefile = validated_data.get('imagefile')
+        user = self.context['request'].user
+
+        # S3에 파일 업로드
+        s3 = boto3.client(
+            's3',
+            aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
+            aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),
+        )
+        file_count = Letter.objects.filter(sender=user).count()
+        file_name = f'{user.id}/{file_count + 1}.jpg'
+        try:
+            s3.upload_fileobj(
+                imagefile,
+                os.environ.get('AWS_STORAGE_BUCKET_NAME'),
+                file_name,
+            )
+        except ClientError as e:
+            print(f"Error uploading file to S3: {e}")
+            raise serializers.ValidationError("파일 업로드에 실패했습니다.")
+
+        try:
+            match = Match.objects.get(requester=user)
+            letter = Letter.objects.create(
+                receiver=match.acceptor,
+                sender=user,
+                image_url=file_name,
+                is_read=False,
+                match_id=match.id,
+            )
+            letter.save()
+        except Match.DoesNotExist:
+            print(f"Match object not found for user: {user}")
+            raise serializers.ValidationError("매칭 정보를 찾을 수 없습니다.")
+        except Exception as e:
+            print(f"Error saving letter: {e}")
+            raise serializers.ValidationError("편지 저장에 실패했습니다.")
+
+        return validated_data
 
 
 class EpsonConnectEmailSerializer(serializers.Serializer):
