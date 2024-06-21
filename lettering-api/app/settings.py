b1bb26e86c4c2a33591db822748620eea2d044c4
minesp3164 <lhd000721@gmail.com>
2024-06-21 22:11:54 +0900

    chore: SSL 에러 해결을 위한 epsonAPI CORS 허용 처리

--- lettering-api/app/settings.py
+++ lettering-api/app/settings.py
@@ -153,6 +153,7 @@ AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
 
 CORS_ALLOWED_ORIGINS = [
     "http://galaxy4276.asuscomm.com:3000",
+    "https://api.epsonconnect.com",
     "http://localhost:3000",
     "http://localhost:8000",
 ]
