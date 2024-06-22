513a89c7e4e3fcd3d85edba67e58e414b538695e
galaxy4276 <deveungi@gmail.com>
2024-06-22 21:44:48 +0900

    fix: 편지는 여러 명의 사용자를 가질 수 있습니다

--- lettering-api/letters/models.py
+++ lettering-api/letters/models.py
@@ -4,8 +4,8 @@ from matching.models import Match
 
 
 class Letter(models.Model):
-    sender = models.OneToOneField(User, related_name='sent_letters', on_delete=models.CASCADE)
-    receiver = models.OneToOneField(User, related_name='received_letters', on_delete=models.CASCADE)
+    sender = models.ForeignKey(User, related_name='sent_letters', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, related_name='received_letters', on_delete=models.CASCADE)
     image_url = models.CharField(null=False)
     is_read = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
