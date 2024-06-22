366e30fb9ff6874c3458d995b215851db51cf507
galaxy4276 <deveungi@gmail.com>
2024-06-23 05:45:59 +0900

    fix: API 요구사항에 맞게 수정

--- lettering-api/notifications/views.py
+++ lettering-api/notifications/views.py
@@ -5,6 +5,8 @@ from rest_framework import status
 from drf_yasg.utils import swagger_auto_schema
 from .models import Notification
 from .serializers import NotificationSerializer
+from typing import List
+
 
 class NotificationAPIView(APIView):
     permission_classes = [IsAuthenticated]
@@ -15,7 +17,7 @@ class NotificationAPIView(APIView):
     )
     def get(self, request):
         user = request.user
-        notifications = Notification.objects.filter(user=user).all()
+        notifications = Notification.objects.filter(user=user, is_read=False).all()
         serializer = NotificationSerializer(notifications, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -27,14 +29,12 @@ class NotificationDetailAPIView(APIView):
         operation_summary="알림 읽음 처리",
         responses={200: "알림 읽음 처리 완료", 404: "알림을 찾을 수 없음"}
     )
-    def put(self, request, notification_id):
+    def put(self, request):
         try:
-            notification = Notification.objects.get(id=notification_id, user=request.user)
+            ids: List[int] = request.data.get('ids', [])
+            Notification.objects.filter(pk__in=ids).update(is_read=True)
         except Notification.DoesNotExist:
             return Response({"detail": "알림을 찾을 수 없음"}, status=status.HTTP_404_NOT_FOUND)
-
-        notification.is_read = True
-        notification.save()
         return Response({"message": "알림 읽음 처리 완료"}, status=status.HTTP_200_OK)
 
     @swagger_auto_schema(
