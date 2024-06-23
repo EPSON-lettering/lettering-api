3b0738bb65422d22ad2aeb3d0546c3368a80d842
galaxy4276 <deveungi@gmail.com>
2024-06-24 01:06:56 +0900

    fix 구조적으로 불가능한 모델이다

--- lettering-api/printers/models.py
+++ lettering-api/printers/models.py
@@ -3,6 +3,14 @@ from accounts.models import User
 from letters.models import Letter
 
 
+'''
+    @deprecated 해당 모델을 활용할 수 없습니다. 
+    전체적인 요구사항이 불일치합니다
+'''
 class EpsonConnectScanData(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     imageUrl = models.FileField(upload_to='letters/')
+
+
+class EpsonGlobalImageShare(models.Model):
+    image_url = models.CharField()
