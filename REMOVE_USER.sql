956fcec70fb8da5fad0781eef5d697aa3fba8ff3
galaxy4276 <deveungi@gmail.com>
2024-06-26 09:00:26 +0900

    chore: 사용자 관련 데이터를 모두 제거하는 SQL 파일. (노가다 금지)

---
+++ REMOVE_USER.sql
@@ -0,0 +1,38 @@
+DO $$
+    DECLARE
+        p_user_id INT;
+        p_match_id INT;
+    BEGIN
+        SELECT u.id INTO p_user_id
+        FROM accounts_user u
+        WHERE u.nickname = '아직도내가은기로보여' # 이부분 수정해주세요
+        LIMIT 1;
+
+        SELECT m.id INTO p_match_id
+        FROM matching_match as m
+        WHERE m.requester_id = p_user_id or m.acceptor_id = p_user_id
+        ORDER BY m.created_at DESC
+        LIMIT 1;
+
+        RAISE NOTICE 'match id: %', p_match_id;
+
+        DELETE FROM matching_match_provided_questions as pq
+        WHERE pq.match_id = p_match_id;
+
+        DELETE FROM matching_match as m
+        WHERE m.id = p_match_id or m.requester_id = p_user_id or m.acceptor_id = p_user_id;
+
+        DELETE FROM matching_matchrequest mr
+        WHERE mr.requester_id = p_user_id or mr.receiver_id = p_user_id;
+
+        DELETE FROM interests_userinterest ui
+        WHERE ui.user_id = p_user_id;
+
+        DELETE FROM token_blacklist_outstandingtoken out_t
+        WHERE out_t.user_id = p_user_id;
+
+        DELETE FROM accounts_user u
+        WHERE u.id = p_user_id;
+
+        RAISE NOTICE '% 번 사용자 데이터가 완전히 제거되었습니다.', p_user_id;
+END $$;
