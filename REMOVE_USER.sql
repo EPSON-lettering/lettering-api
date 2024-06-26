ae2bb8fa69863b57c5fb186c82f89f9159e51802
west_east <west.east1832@gmail.com>
2024-06-26 14:00:00 +0900

    fix: 유저 삭제 sql 참조 무결성

--- REMOVE_USER.sql
+++ REMOVE_USER.sql
@@ -3,41 +3,42 @@ DO $$
         p_user_id INT;
         p_match_id INT;
     BEGIN
-
-        DELETE FROM oauth_oauthuser oauth
-        WHERE oauth.provider_id = 'west.east1832@gmail.com'; -- 이부분 수정해주세요
-
-
         SELECT u.id INTO p_user_id
         FROM accounts_user u
-        WHERE u.nickname = '동현' -- 이부분 수정해주세요
+        WHERE u.nickname = '동현111' -- 이 부분 수정해주세요
         LIMIT 1;
 
         SELECT m.id INTO p_match_id
         FROM matching_match as m
-        WHERE m.requester_id = p_user_id or m.acceptor_id = p_user_id
+        WHERE m.requester_id = p_user_id OR m.acceptor_id = p_user_id
         ORDER BY m.created_at DESC
         LIMIT 1;
 
         RAISE NOTICE 'match id: %', p_match_id;
 
-        DELETE FROM matching_match_provided_questions as pq
-        WHERE pq.match_id = p_match_id;
+        DELETE FROM matching_match_provided_questions
+        WHERE match_id IN (
+            SELECT id FROM matching_match
+            WHERE requester_id = p_user_id OR acceptor_id = p_user_id
+        );
 
-        DELETE FROM matching_match as m
-        WHERE m.id = p_match_id or m.requester_id = p_user_id or m.acceptor_id = p_user_id;
+        DELETE FROM matching_match
+        WHERE id = p_match_id OR requester_id = p_user_id OR acceptor_id = p_user_id;
 
-        DELETE FROM matching_matchrequest mr
-        WHERE mr.requester_id = p_user_id or mr.receiver_id = p_user_id;
+        DELETE FROM matching_matchrequest
+        WHERE requester_id = p_user_id OR receiver_id = p_user_id;
 
-        DELETE FROM interests_userinterest ui
-        WHERE ui.user_id = p_user_id;
+        DELETE FROM interests_userinterest
+        WHERE user_id = p_user_id;
 
-        DELETE FROM token_blacklist_outstandingtoken out_t
-        WHERE out_t.user_id = p_user_id;
+        DELETE FROM token_blacklist_outstandingtoken
+        WHERE user_id = p_user_id;
 
-        DELETE FROM accounts_user u
-        WHERE u.id = p_user_id;
+        DELETE FROM accounts_user
+        WHERE id = p_user_id;
+
+        DELETE FROM oauth_oauthuser
+        WHERE provider_id = 'west.east1832@gmail.com'; -- 이 부분 수정해주세요
 
         RAISE NOTICE '% 번 사용자 데이터가 완전히 제거되었습니다.', p_user_id;
-END $$;
+    END $$;
