a772317acb701c4fb23b13cc863bba8c25ba6777
minesp3164 <lhd000721@gmail.com>
2024-06-28 22:52:50 +0900

    feat: 매치 상대 뱃지 데이터 가져오기 API

--- lettering-api/badges/urls.py
+++ lettering-api/badges/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 
-from .views import UserBadgeAPIView, BadgeAPIView
+from .views import UserBadgeAPIView, BadgeAPIView, PartnerBadgeAPIView
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -16,4 +16,5 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('',UserBadgeAPIView.as_view() , name='badges'),
     path('all/',BadgeAPIView.as_view(), name='badges'),
+    path('partner/', PartnerBadgeAPIView.as_view(), name='badges'),
 ]
--- lettering-api/badges/views.py
+++ lettering-api/badges/views.py
@@ -3,6 +3,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from drf_yasg.utils import swagger_auto_schema
+
+from accounts.models import User
+from matching.models import Match
 from .serializers import UserBadgeSerializer, BadgeSerializer
 from .models import UserBadge, Badge
 
@@ -18,6 +21,32 @@ class BadgeAPIView(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
+class PartnerBadgeAPIView(APIView):
+    @swagger_auto_schema(
+        operation_summary="대화 상대 뱃지 조회"
+    )
+    def get(self, request):
+        print(request.user)
+        user = request.user
+        match = (Match.objects
+                 .filter(acceptor=user, withdraw_reason__isnull=True)
+                 .order_by("-created_at")
+                 .first()
+                 )
+        partner = match.requester
+        if match is None:
+            match = (Match.objects
+                     .filter(requester=user, withdraw_reason__isnull=True)
+                     .order_by("-created_at")
+                     .first()
+                     )
+            partner = match.acceptor
+        user_badges = UserBadge.objects.filter(user=partner)
+        serializer = UserBadgeSerializer(user_badges, many=True)
+        print(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
 class UserBadgeAPIView(APIView):
     permission_classes = [IsAuthenticated]
 
@@ -28,4 +57,5 @@ class UserBadgeAPIView(APIView):
     def get(self, request):
         user_badges = UserBadge.objects.filter(user=request.user)
         serializer = UserBadgeSerializer(user_badges, many=True)
+        print(serializer.data)
         return Response(serializer.data, status=status.HTTP_200_OK)
